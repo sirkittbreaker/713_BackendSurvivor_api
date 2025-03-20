6e4b938e23229efd4d5645ff224ba42bc46a5056
sirkittbreaker <kittienan.kitkiki@gmail.com>
2025-03-20 12:22:10 +0700

    Add allow origins

--- src/server.ts
+++ src/server.ts
@@ -3,17 +3,25 @@ import authRoute from "./routes/authRoute";
 import teacherRoute from "./routes/teacherRoute";
 import studentRoute from "./routes/studentRoute";
 import appointmentRoute from "./routes/appointmentRoute";
+import dotenv from "dotenv";
+import cors from "cors";
 
+dotenv.config();
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(",") || [];
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
 
 const app = express();
+app.use(cors(options));
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
 app.use("/api/v1/auth", authRoute);
-app.use('/api/v1/students', studentRoute);
-app.use('/api/v1/teachers', teacherRoute);
-app.use('/api/v1/appointments', appointmentRoute);
-
+app.use("/api/v1/students", studentRoute);
+app.use("/api/v1/teachers", teacherRoute);
+app.use("/api/v1/appointments", appointmentRoute);
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
