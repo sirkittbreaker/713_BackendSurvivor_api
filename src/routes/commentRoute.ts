7336aa20f29d22157255a90c4662f867b31e2cb2
sirkittbreaker <kittienan.kitkiki@gmail.com>
2025-03-24 10:49:22 +0700

    refactor res body

--- src/routes/commentRoute.ts
+++ src/routes/commentRoute.ts
@@ -141,7 +141,7 @@ router.get(
         teacherId,
         studentId
       );
-      res.json({ comments });
+      res.json(comments);
     } catch (error) {
       console.error("❌", error);
       res.status(500).json({ message: "Internal server error" });
