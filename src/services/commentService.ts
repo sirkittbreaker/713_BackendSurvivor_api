1b77a07588ca792574747026d25bbcd0e1c65c22
Gap <puri.pongsomboonsuk@gmail.com>
2025-03-24 23:11:53 +0700

    Update schema and comment routes

--- src/services/commentService.ts
+++ src/services/commentService.ts
@@ -3,9 +3,10 @@ import * as repo from "../repositories/commentRepository";
 export async function addComment(
   teacherId: number,
   studentId: string,
-  content: string
+  content: string,
+  userId: number
 ) {
-  const newComment = await repo.addComment(teacherId, studentId, content);
+  const newComment = await repo.addComment(teacherId, studentId, content, userId);
   return newComment;
 }
 
@@ -13,13 +14,15 @@ export async function addReply(
   commentId: number,
   studentId: string,
   teacherId: number,
-  content: string
+  content: string,
+  userId: number
 ) {
   const newReply = await repo.addReply(
     commentId,
     studentId,
     teacherId,
-    content
+    content,
+    userId
   );
   return newReply;
 }
