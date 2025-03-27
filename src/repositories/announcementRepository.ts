21b8cae572f430b49c90e4cdfd55a37cd8c63048
sirkittbreaker <kittienan.kitkiki@gmail.com>
2025-03-27 21:37:05 +0700

    Add get latest announcement

--- src/repositories/announcementRepository.ts
+++ src/repositories/announcementRepository.ts
@@ -2,27 +2,56 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-
 export async function getAllAnnouncementsByTeacherId(teacherId: number) {
-    const announcements = await prisma.announcement.findMany({
-        where: {
-            teacherId: teacherId,
-        },
-    });
+  const announcements = await prisma.announcement.findMany({
+    where: {
+      teacherId: teacherId,
+    },
+  });
 
-    return announcements;
+  return announcements;
 }
 
+export async function addAccouncement(
+  title: string,
+  content: string,
+  fileUrl: string,
+  teacherId: number
+) {
+  const announcement = await prisma.announcement.create({
+    data: {
+      title: title,
+      content: content,
+      fileUrl: fileUrl,
+      teacherId: teacherId,
+    },
+  });
 
-export async function addAccouncement(title: string, content: string,fileUrl: string, teacherId: number) {
-    const announcement = await prisma.announcement.create({
-        data: {
-            title: title,
-            content: content,
-            fileUrl: fileUrl,
-            teacherId: teacherId,
+  return announcement;
+}
+
+export async function getLatestAnnouncementByTeacherId(teacherId: number) {
+  const announcement = await prisma.announcement.findFirst({
+    where: {
+      teacherId: teacherId,
+    },
+    orderBy: {
+      createdAt: "desc", // Sort by newest first
+    },
+    include: {
+      teacher: {
+        select: {
+          firstName: true,
+          lastName: true,
+          academicPosition: {
+            select: {
+              title: true,
+            },
+          },
         },
-    });
+      },
+    },
+  });
 
-    return announcement;
+  return announcement;
 }
